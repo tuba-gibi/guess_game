754dc60a3d96032e0457e9695f9240576b211d81
tuba-gibi <eetede84@gmail.com>
2023-01-14 16:36:04 +0300

    js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const n = Math.round(Math.random()*100)
+
 console.log(n);
 
-const guessNum = document.getElementsByTagName("input").value
-console.log(guessNum);
+const guessNum = document.getElementsByTagName("input")
+// console.log(guessNum);
 const value = +prompt ("Enter a number")
+let x = guessNum.value;
+let y = n
+if(0 < x && x < 100 ) {
+
+}
