dac1a23442ddb781edefad25c25ad89e544629fb
tuba-gibi <eetede84@gmail.com>
2023-01-14 16:32:36 +0300

    js

---
+++ app.js
@@ -0,0 +1,6 @@
+const n = Math.round(Math.random()*100)
+console.log(n);
+
+const guessNum = document.getElementsByTagName("input").value
+console.log(guessNum);
+const value = +prompt ("Enter a number")
